5691b3885404abc04be4d2b4d7c82706da26826d
mifmasterz <mifmasterz@yahoo.com>
2022-01-23 06:47:54 +0700

    fixing connection bmp280 demo

--- src/MeasureTempApp/Program.cs
+++ src/MeasureTempApp/Program.cs
@@ -1,4 +1,5 @@
 using Iot.Device.Bmxx80;
+using nanoFramework.Hardware.Esp32;
 using System;
 using System.Device.I2c;
 using System.Diagnostics;
@@ -22,25 +23,31 @@ namespace MeasureTempApp
 
         static void Loop()
         {
+            //for ESP 32
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO22, DeviceFunction.I2C1_CLOCK);
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO21, DeviceFunction.I2C1_DATA);
+
             // I2C bus 1 is using GPIO 18 and GPIO 19 on the ESP32
             const int busId = 1;
-            I2cConnectionSettings i2cSettings = new(busId, Bmp280.DefaultI2cAddress);
+            I2cConnectionSettings i2cSettings = new(busId, Bmp280.SecondaryI2cAddress,I2cBusSpeed.StandardMode);
             I2cDevice i2cDevice = I2cDevice.Create(i2cSettings);
-            var i2CBmp280 = new Bmp280(i2cDevice);
-            // set higher sampling
-            i2CBmp280.TemperatureSampling = Sampling.LowPower;
-            i2CBmp280.PressureSampling = Sampling.UltraHighResolution;
+                var i2CBmp280 = new Bmp280(i2cDevice);
 
-            
-            while (true)
-            {
-                var readResult = i2CBmp280.Read();
-                if (readResult != null)
+                // set higher sampling
+                i2CBmp280.TemperatureSampling = Sampling.LowPower;
+                i2CBmp280.PressureSampling = Sampling.UltraHighResolution;
+
+
+                while (true)
                 {
-                    Debug.WriteLine($"Temp: {readResult.Temperature.DegreesCelsius} C, Pressure: {readResult.Pressure.Bars} bar");
+                    var readResult = i2CBmp280.Read();
+                    if (readResult != null)
+                    {
+                        Debug.WriteLine($"Temp: {readResult.Temperature.DegreesCelsius} C, Pressure: {readResult.Pressure.Bars} bar");
+                    }
+                    Thread.Sleep(500);
                 }
-                Thread.Sleep(500);
-            }
+            
         }
     }
 }
